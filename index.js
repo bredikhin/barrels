5a1fdb8af0e308c514b3f5ff4f42540c961101f3
Ruslan Bredikhin <rus.brn@gmail.com>
2014-09-03 22:31:59 -0400

    refactor api

--- index.js
+++ index.js
@@ -1,42 +1,60 @@
+'use strict';
+
 /**
- * Simple fixtures for Sails.js
+ * Barrels: Simple fixtures for Sails.js
  */
 
 /**
  * Module dependencies
  */
-var fs = require('fs')
-  , path = require('path')
-  , async = require('async')
-  , _ = require('lodash')
-  , data = {};
+var fs = require('fs');
+var path = require('path');
+var async = require('async');
+var _ = require('lodash');
+
+module.exports = Barrels;
 
 /**
- * Perform the actual pupulation of the Sails Models.
- * Note we need to destroy any existing data in the models,
- * before inserting the fixtures.
- * @param  {Function} done callback function with signature done(err, result)
+ * Barrels module
+ * @param {string} sourceFolder defaults to <project root>/test/fixtures
  */
-function doPopulateSails(done) {
-  var modelNames = Object.keys(data);
+function Barrels(sourceFolder) {
+  if (!(this instanceof Barrels))
+    return new Barrels(sourceFolder);
 
-  // populate sails wiht each model (in parallel using async)
+  // Fixture objects loaded from the JSON files
+  this.data = {};
+
+  sourceFolder = sourceFolder || process.cwd() + '/test/fixtures';
+  var files = fs.readdirSync(sourceFolder);
+
+  for (var i = 0; i < files.length; i++) {
+    if (path.extname(files[i]).toLowerCase() === '.json')
+    {
+      var modelName = path.basename(files[i]).split('.')[0].toLowerCase();
+      this.data[modelName] = require(path.join(sourceFolder, files[i]));
+    }
+  }
+}
+
+Barrels.prototype.populate = function(done) {
+  var modelNames = Object.keys(this.data);
+  var that = this;
+
+  // Populate each table / collection
   async.each(modelNames, function(modelName, nextModel) {
     var Model = sails.models[modelName];
     if (Model) {
-
       //Cleanup existing data in the model
       Model.destroy({}, function(err) {
-
-        // Insert all items from the fixture in the model (in parallel using async)
-        async.each(_.cloneDeep(data[modelName]), function(item, nextItem) {
-          if (err) {
+        // Insert all items from the fixture in the model
+        var fixtureObjects = _.cloneDeep(that.data[modelName]);
+        async.each(fixtureObjects, function(item, nextItem) {
+          if (err)
             return done(err);
-          }
           Model.create(item, function(err) {
-            if (err) {
+            if (err)
               return done(err);
-            }
             nextItem();
           });
         }, nextModel);
@@ -46,51 +64,3 @@ function doPopulateSails(done) {
     }
   }, done);
 }
-
-module.exports = {
-  objects: data, // Fixtures dumped after loading
-
-  /**
-   * Read fixtures from JSON files into `objects`
-   * @param  {String} [folder='<project_root>/test/fixtures'] path to fixtures
-   * @return {Object}        this module, where the objects member is holding loaded fixtures
-   */
-  load: function (folder) {
-    //folder defaults to <project root>/test/fixtures
-    folder = folder || process.cwd() + '/test/fixtures';
-    var files = fs.readdirSync(folder);
-    var i;
-
-    for (i = 0; i < files.length; i++) {
-      if (path.extname(files[i]).toLowerCase() === '.json')
-      {
-        var modelName = path.basename(files[i]).split('.')[0].toLowerCase();
-        data[modelName] = require(path.join(folder, files[i]));
-      }
-    }
-
-    return this;
-  },
-
-  /**
-   * Populate the sails model with the fixture data.
-   * Ensure that fixtures has;
-   * @param  {Function} done   callback function with signature done(err, result)
-   * @param  {String}   [folder] path to fixture, only used if fixtures is not loaded yet
-   */
-  populate: function (done, folder) {
-    // We need to make sure that data hasBeen loaded
-    if (Object.keys(data).length > 0) {
-      doPopulateSails(done);
-    } else {
-      this.load(folder);
-
-      // validate that fixtures has been loaded into data
-      if (Object.keys(data).length > 0) {
-        doPopulateSails(done);
-      }
-      else
-        done();
-    }
-  }
-};
