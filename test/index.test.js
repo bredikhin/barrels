608e3da469185d18bdd280dee9b764805d726dba
Ruslan Bredikhin <rus.brn@gmail.com>
2014-10-15 23:00:16 -0400

    add disabled auto-associations test

--- test/index.test.js
+++ test/index.test.js
@@ -13,13 +13,15 @@ describe('Barrels', function() {
 
   // Load fixtures into memory
   describe('constructor', function() {
-    it ('should load all the json files from default folder', function() {
-      Object.keys(fixtures).length.should.be.greaterThan(1, 'At least two fixture files should be loaded!');
+    it('should load all the json files from default folder', function() {
+      Object.keys(fixtures).length.should.be.greaterThan(1,
+        'At least two fixture files should be loaded!');
     });
 
-    it ('should set generate lowercase property names for models', function() {
+    it('should set generate lowercase property names for models', function() {
       var oneWord = Object.keys(fixtures).join();
-      oneWord.toLowerCase().should.be.eql(oneWord, 'Property names should be in lowercase!');
+      oneWord.toLowerCase().should.be.eql(oneWord,
+        'Property names should be in lowercase!');
     });
   });
 
@@ -31,7 +33,8 @@ describe('Barrels', function() {
           level: 'error'
         },
         paths: {
-          models: require('path').join(process.cwd(), 'test/fixtures/models')
+          models: require('path').join(process.cwd(),
+            'test/fixtures/models')
         },
         connections: {
           test: {
@@ -46,10 +49,7 @@ describe('Barrels', function() {
           grunt: false
         }
       }, function(err, sails) {
-        if (err)
-          return done(err);
-
-        barrels.populate(done);
+        done(err);
       });
     });
 
@@ -57,49 +57,80 @@ describe('Barrels', function() {
       sails.lower(done);
     });
 
-    it ('should populate the DB with products and categories', function(done) {
-      Categories.find().exec(function(err, categories) {
-        if (err)
-          return done(err);
+    describe('populate(cb)', function() {
+      before(function(done) {
+        barrels.populate(done);
+      });
 
-        var gotCategories = (fixtures['categories'].length > 0);
-        var categoriesAreInTheDb = (categories.length === fixtures['categories'].length);
-        should(gotCategories&&categoriesAreInTheDb).be.ok;
-
-        Products.find().exec(function(err, products) {
+      it('should populate the DB with products and categories', function(done) {
+        Categories.find().exec(function(err, categories) {
           if (err)
             return done(err);
 
-          categories.length.should.be.eql(products.length,
-            'Categories and products should have equal amount of entries!');
-          done();
+          var gotCategories = (fixtures['categories'].length >
+            0);
+          var categoriesAreInTheDb = (categories.length ===
+            fixtures['categories'].length);
+          should(gotCategories && categoriesAreInTheDb).be.ok;
+
+          Products.find().exec(function(err, products) {
+            if (err)
+              return done(err);
+
+            categories.length.should.be.eql(products.length,
+              'Categories and products should have equal amount of entries!'
+            );
+            done();
+          });
+        });
+      });
+
+      it('should assign a category to each product', function(done) {
+        Products.find().populate('category').exec(function(err,
+          products) {
+          if (err)
+            return done(err);
+
+          async.each(products, function(product,
+            nextProduct) {
+            should(product.category.name).not.be.empty;
+
+            nextProduct();
+          }, done);
+        });
+      });
+
+      it('should assign at least two tags to each product', function(done) {
+        Products.find().populate('tags').exec(function(err, products) {
+          if (err)
+            return done(err);
+
+          async.each(products, function(product, nextProduct) {
+            should(product.tags.length).be.greaterThan(1);
+
+            nextProduct();
+          }, done);
         });
       });
     });
 
-    it ('should assign a category to each product', function(done) {
-      Products.find().populate('category').exec(function(err, products) {
-        if (err)
-          return done(err);
-
-        async.each(products, function(product, nextProduct) {
-          should(product.category.name).not.be.empty;
-
-          nextProduct();
-        }, done);
+    describe('populate(cb, false)', function() {
+      before(function(done) {
+        barrels.populate(done, false);
       });
-    });
 
-    it ('should assign at least two tags to each product', function(done) {
-      Products.find().populate('tags').exec(function(err, products) {
-        if (err)
-          return done(err);
+      it('should keep the associations-related fields', function(done) {
+        Products.find().exec(function(err, products) {
+          if (err)
+            return done(err);
 
-        async.each(products, function(product, nextProduct) {
-          should(product.tags.length).be.greaterThan(1);
+          async.each(products, function(product, nextProduct) {
+            product.category.should.be.a.Number;
+            product.tags.should.be.an.Array;
 
-          nextProduct();
-        }, done);
+            nextProduct();
+          }, done);
+        });
       });
     });
   });
