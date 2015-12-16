e571d49d231e704c0e7d3d6e4de0349eb35f9338
Peter Kamps <volkljibster@gmail.com>
2015-12-16 14:44:23 +0100

    Wrote tests for models with string ids (to mock MongoDB ids).

---
+++ test/fixtures/string-id/Categories.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: '1',
+    name: 'Clothes'
+  },
+  {
+    id: '2',
+    name: "Shoes"
+  },
+  {
+    id: '3',
+    "name": 'Accessories'
+  }
+]
---
+++ test/fixtures/string-id/models/Sellers.js
@@ -0,0 +1,17 @@
+/**
+ * Sellers
+ */
+
+module.exports = {
+    attributes: {
+        id: {
+            type: 'string',
+            primaryKey: true,
+            required: true
+        },
+        name: {
+            type: 'string',
+            required: true
+        }
+    }
+};
