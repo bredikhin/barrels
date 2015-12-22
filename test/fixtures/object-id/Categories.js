a8b88a5f2ac299e3799a11ab1bf0ba11816d728c
Peter Kamps <volkljibster@gmail.com>
2015-12-22 22:08:42 +0100

    Added completely new test case, which has MongoDB-like IDs (string ObjectId's).

---
+++ test/fixtures/object-id/Categories.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: '507f1f77bcf86cd799439011',
+    name: 'Clothes'
+  },
+  {
+    id: '507f191e810c19729de860ea',
+    name: "Shoes"
+  },
+  {
+    id: '56799fa7fdafa4f9402ebb06',
+    "name": 'Accessories'
+  }
+]
